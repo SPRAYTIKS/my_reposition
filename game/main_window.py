f4350c47a7ece2a393b6558a988cdde66e92a261
Akim_Kuricyn <drakosha23akim@yandex.com>
2025-01-21 22:33:03 +0300

    add sound adjustment

--- game/main_window.py
+++ game/main_window.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import pygame
+import pygame_widgets
+from pygame_widgets.button import Button
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 
 
 def load_image(name, color_key=None):
@@ -12,69 +16,66 @@ def load_image(name, color_key=None):
         raise SystemExit(message)
     image = image.convert_alpha()
     if color_key is not None:
-        if color_key is -1:
+        if color_key == -1:
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
     return image
-pygame.init()
 
+
+# Создаем переменные громкостей музыки и эффектов
+music_value = 0
+effects_value = 0
+pygame.init()
 
 if __name__ == "__main__":
     pygame.init()
     size = width, height = 1000, 600
     screen = pygame.display.set_mode(size)
-    intro_text = ["Приветствую тебя, новобранец, ты попал в Battle of Heroes", "", "", ""]
 
     fon = pygame.transform.scale(load_image('start_fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
-    font_45 = pygame.font.Font(None, 45)
+    btn_play = Button(screen, 400, 150, 200, 100, text='Play', fontSize=50, margin=20,
+                      inactiveColour=(200, 50, 0), hoverColour=(150, 0, 0),
+                      overpressedColour=(0, 200, 20),
+                      radius=20,
+                      onClick=lambda: print('Click')
+                      )
+
+    intro_text = ["Музыка", "Эффекты", ]
     font_30 = pygame.font.Font(None, 30)
-    text_coord = 75
-    string_rendered = font_45.render(intro_text[0], 1, pygame.Color('black'))
-    intro_rect = string_rendered.get_rect()
-    intro_rect.top = text_coord - 10
-    intro_rect.x = 60
-    text_coord += intro_rect.height
-    screen.blit(string_rendered, intro_rect)
-    text_coord = 75
+    text_coord = 73
+    for i in range(2):
+        string_rendered = font_30.render(intro_text[i], 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        intro_rect.top = text_coord - 10
+        intro_rect.x = 860 - i * 30
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+        text_coord += 75
 
-    all_sprites = pygame.sprite.Group()
-    button = pygame.sprite.Sprite()
-    button.image = load_image("btn_start.png", 'white')
-    button.rect = button.image.get_rect()
+    slider_m = Slider(screen, 840, 10, 150, 20, min=0, max=99, step=1, initial=0)
+    gromkosti_m = TextBox(screen, 950, 50, 45, 40, fontSize=30)
+    gromkosti_m.disable()
+    font = pygame.font.Font(None, 50)
+    text = font.render("Музыка", True, (100, 255, 100))
 
-    im = load_image('btn_3.png')
-    im = pygame.transform.scale(im, (100, 100))
-    button_win3 = pygame.sprite.Sprite()
-    button_win3.image = im
-    button_win3.rect = button_win3.image.get_rect()
-
-    im = load_image('btn_1.png')
-    im = pygame.transform.scale(im, (100, 100))
-    button_win1 = pygame.sprite.Sprite()
-    button_win1.image = im
-    button_win1.rect = button_win3.image.get_rect()
-
-
-    button.rect.x = 340
-    button.rect.y = 130
-
-    button_win3.rect.x = 350
-    button_win3.rect.y = 250
-
-    button_win1.rect.x = 540
-    button_win1.rect.y = 250
-    all_sprites.add(button)
-    all_sprites.add(button_win3)
-    all_sprites.add(button_win1)
-    all_sprites.draw(screen)
+    slider_e = Slider(screen, 840, 110, 150, 20, min=0, max=99, step=1, initial=0)
+    gromkosti_e = TextBox(screen, 950, 150, 45, 40, fontSize=30)
+    gromkosti_e.disable()
+    font = pygame.font.Font(None, 50)
+    text = font.render("Эффекты", True, (100, 255, 100))
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            #if event.type == pygame.MOUSEDOWN:
-
+            # if event.type == pygame.MOUSEDOWN:
+            pygame_widgets.update(event)
         pygame.display.flip()
+        gromkosti_m.setText(slider_m.getValue())
+        gromkosti_e.setText(slider_e.getValue())
+        # Обновляем громкость звуков
+        music_value = slider_m.getValue()
+        effects_value = slider_e.getValue()
     pygame.quit()
